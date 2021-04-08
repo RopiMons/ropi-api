a9e74a73048b12b5d89cc8fab48325dc06de2e2a
Laurent Cardon <laurent.cardon@jsb.be>
2021-04-08 12:33:20 +0200

    Mise à jour des liens images

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -14,7 +14,7 @@ class ArticleFixtures extends Fixture
         $array = [
             'Billet 10' => [
                 'nom' => '10 Ropi',
-                'image' => 'article/g0jop3vo.bmp',
+                'image' => 'images/article/g0jop3vo.bmp',
                 'actif' => true,
                 'description' => 'Billet de 10 Ropi',
                 'prix' => 10,
@@ -22,7 +22,7 @@ class ArticleFixtures extends Fixture
             ],
             'Billet 5' => [
                 'nom' => '5 Ropi',
-                'image' => 'article/lgt8ddel.bmp',
+                'image' => 'images/article/lgt8ddel.bmp',
                 'actif' => true,
                 'description' => 'Billet de 5 Ropi',
                 'prix' => 5,
@@ -30,7 +30,7 @@ class ArticleFixtures extends Fixture
             ],
             'Billet 1' => [
                 'nom' => '1 Ropi',
-                'image' => 'article/sd78rlag.bmp',
+                'image' => 'images/article/sd78rlag.bmp',
                 'actif' => true,
                 'description' => 'Billet de 1 Ropi',
                 'prix' => 1,
@@ -38,7 +38,7 @@ class ArticleFixtures extends Fixture
             ],
             'Billet 05' => [
                 'nom' => '1/2 Ropi',
-                'image' => 'article/b56uvy5v.bmp',
+                'image' => 'images/article/b56uvy5v.bmp',
                 'actif' => true,
                 'description' => 'Billet de 0,5 Ropi',
                 'prix' => 0.5,
