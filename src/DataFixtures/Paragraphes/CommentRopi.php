d1400662e6d704386ed1ab23516af64599a7b592
Fabian Dortu <fdortu@fastmail.net>
2021-05-03 13:42:01 +0200

    mise à jour contenu, ongoing...

--- src/DataFixtures/Paragraphes/CommentRopi.php
+++ src/DataFixtures/Paragraphes/CommentRopi.php
@@ -1,6 +1,6 @@
 <?php
 
-return  "
+return "
 
 <br>
 <br>
@@ -21,27 +21,29 @@ return  "
 <tbody>
 <tr>
 <td><img src=\"https://localhost:8000/images/cms/component_comptoir.png\" alt=\"Comptoir d'échange\" width=\"100\"  /></td>
-<td><h4><strong>&nbsp; Des citoyens consom'acteurs décident de se procurer des Ropi</strong></h4></td>
+<td><h3><strong>&nbsp; Des citoyens consom'acteurs décident de se procurer des Ropi</strong></h3></td>
 </tr>
 </tbody>
 </table>
 
 <br>
 
-<h5 style=\"line-height: 200%; text-align: center; padding-left: 30px;\">
+<ul>
+<li>
+<h4 style=\"line-height: 200%; padding-left: 30px;\">
 <strong>En commandant des Ropi <a href=\"../../../../page/Actions/Commander%20des%20Ropi\">par internet</a> </strong>
 (paiement par virement bancaire), et se les faire livrer dans un commerce de son choix ou à domicile
 (livraison par un bénévole, le coursier montois ou par la poste). L'achat par internet est encouragé 
-car il permet de grandir la masse de Ropi en circulation, et par conséquent le fonds de garantie en euros.</5>
-
-<br>
-
-<h5 style=\"line-height: 200%; text-align: center; padding-left: 30px;\">
+car il permet de grandir la masse de Ropi en circulation, et par conséquent le fonds de garantie en euros.</h4>
+</li>
+<li>
+<h4 style=\"line-height: 200%; padding-left: 30px;\">
 <strong>En se rendant chez un <a href=\"../../../../commerces\">commerçant qui accepte le Ropi</a></strong>, 
 et lui acheter des Ropi dans la limite de disponibilité de son tiroir-caisse. 
 Cette façon de se procurer des Ropi ne fait pas grandir la masse monétaire en Ropi, 
-mais elle a le mérite d'aider le commerçant à écouler ses Ropi.</h5>
-
+mais elle a le mérite d'aider le commerçant à écouler ses Ropi.</h4>
+</li>
+</ul>
 
 
 <br>
@@ -53,26 +55,26 @@ mais elle a le mérite d'aider le commerçant à écouler ses Ropi.</h5>
 <tbody>
 <tr>
 <td><img src=\"https://localhost:8000/images/cms/component_commercant.png\" alt=\"Commerçant\" width=\"100\"  /></td>
-<td><strong><h4>&nbsp; Les citoyens se rendent chez des prestataire</strong> <strong>acceptant le Ropi</h4></strong></td>
+<td><strong><h3>&nbsp; Les citoyens se rendent chez des prestataire</strong> <strong>acceptant le Ropi</h3></strong></td>
 </tr>
 </tbody>
 </table>
 
 <br>
 
-<h5 style=\"line-height: 200%; text-align: center; padding-left: 30px;\">La <strong>liste des prestataires</strong> 
+<h4 style=\"line-height: 200%; padding-left: 30px;\">La <strong>liste des prestataires</strong> 
 (commerçants, artisants, agriculteurs, prestataires de services, ...) est consultable 
 via <a href=\"../../../../commerces\"><strong>la carte et l'outil de recherche</strong></a> <strong>des commerces</strong>.
 Le plus souvent, les commerçants arboreront aussi un <strong>autocollant en vitrine 
 ou à l'intérieur du magasin</strong> indiquant qu'ils acceptent les Ropi.
-</h5>
+</h4>
 
-
-<h5 style=\"text-align: right;\">&nbsp;<img style=\"display: block; margin-left: auto; margin-right: auto;\" 
+<h4 style=\"text-align: right;\">&nbsp;<img style=\"display: block; margin-left: auto; margin-right: auto;\" 
 src=\"https://github.com/RopiMons/RopiDocs/raw/master/Visuels/AutoCollant/Ropi-autocollant-oui1.png\" 
 alt=\"autocollant\" 
 width=\"400\"  />
-</h5>
+</h4>
+
 
 
 <br>
@@ -86,56 +88,60 @@ width=\"400\"  />
 <tbody>
 <tr>
 <td><img src=\"https://localhost:8000/images/cms/component_producteur.png\" alt=\"Producteurs\" width=\"100\" /></td>
-<td><h4><strong>&nbsp; Les prestataires s'approvisionnent chez des fournisseurs acceptant les Ropi</strong></h4></td>
+<td><h3><strong>&nbsp; Les prestataires s'approvisionnent chez des fournisseurs acceptant les Ropi</strong></h3></td>
 </tr>
 </tbody>
 </table>
 
 <br>
 
-<h5 style=\"line-height: 200%; text-align: center; padding-left: 30px;\"><strong>Les prestataires poursuivent la spirale vertueuse de la relocalisation 
+<h4 style=\"line-height: 200%; padding-left: 30px;\"><strong>Les prestataires poursuivent la spirale vertueuse de la relocalisation 
 de l'économie</strong>, 
 en trouvant d'autres prestataires et producteurs locaux qui acceptent le&nbsp;Ropi. 
 C'est facile grâce à la <strong><a href=\"../../../../commerces\">carte et l'outil de recherche</a></strong> 
 des commerçants et producteurs. 
 
 Et il existe encore bien <a href=\"../../../../page/Ropi/Ecouler%20ses%20Ropi\"><strong>d'autres mani&egrave;res d'écouler ses Ropi</strong>.</a></p>
-<p style=\"padding-left: 30px;\"><strong>S'il n'existe pas de prestataire local adéquat</strong>, les ambassadeurs se feront un plaisir de <strong>rechercher le maillon manquant</strong>. Cependant, tous les usagers sont invités à proposer de nouveaux prestataires en remplissant le formulaire adhoc 
+
+<p style=\";\"><strong>S'il n'existe pas de prestataire local adéquat</strong>, les ambassadeurs se feront un plaisir de <strong>rechercher le maillon manquant</strong>. Cependant, tous les usagers sont invités à proposer de nouveaux prestataires en remplissant le formulaire adhoc 
 (<a href=\"../../../../page/Actions/Proposer%20un%20maillon\">Proposer un maillon</a>).</p>
-<p style=\"padding-left: 30px;\"><strong>S'il n'y a vraiment pas moyen d'écouler ses Ropi</strong>,
+<p style=\"\"><strong>S'il n'y a vraiment pas moyen d'écouler ses Ropi</strong>,
 il est toujours possible de<strong> reconvertir les Ropi en euros</strong>, en s'acquittant d'une <strong>taxe de 5%</strong>. 
 La reconversion n'est possible que pour les membres prestataires.&nbsp;</p>
 <p style=\"padding-left: 60px;\">&rarr; Pour ces démarches, contactez votre ambassadeur ou responsable de zone, 
 ou envoyez un mail à <a href=\"mailto:support@ropi.be\"><code>support@ropi.be</code></a>&nbsp; ou encore, complétez le <a href=\"../../../../page/Actions/proposer un maillon\">formulaire de proposition de maillage</a> en ligne.</p>
-<p style=\"padding-left: 30px;\">Vous l'aurez compris, ce n'est pas du tout le but poursuivi de reconvertir ses Ropi en euros, 
+<p style=\"\">Vous l'aurez compris, ce n'est pas du tout le but poursuivi de reconvertir ses Ropi en euros, 
 et c'est pour cela qu'une taxe de reconversion de 5% est appliquée. Le but recherché est au contraire d'<strong>agrandir le réseau des prestataires afin que les Ropi puissent circuler de prestataires en prestataires</strong> sans jamais rester bloqué chez un prestataire qui ne touverait pas de fili&egrave;re d'approvisionnement locale. La reconversion à 0% est aussi possible <a href=\"Ecouler ses Ropi\">sous certaines conditions</a>.</p>
-<p>&nbsp;</h5>
+<p>&nbsp;</h4>
 
 <br>
 <br>
 <br>
 <br>
 
-<h4 style=\"text-align: center;\"><strong>En conclusion</strong></h4>
+<h3 style=\"text-align: center;\"><strong>En conclusion</strong></h3>
 
 <br>
 
-<h5 style=\"line-height: 200%; text-align: center; \">Ce sont des <strong>citoyens</strong> engagés pour le devenir de leur cité 
+<h4 style=\"line-height: 200%;  \">Ce sont des <strong>citoyens</strong> engagés pour le devenir de leur cité 
 qui <strong>injectent des Ropi dans l'économie locale</strong> en achetant des Ropi 
 et <strong>en s'approvisionnant chez les prestataires locaux</strong> qui acceptent les Ropi. En agissant de la sorte, 
-les citoyens s'attendent que les prestataires leur emboiteront le pas.</h5>
+les citoyens s'attendent que les prestataires leur emboiteront le pas.</h4>
 
-<h5 style=\"line-height: 200%; text-align: center; >Ce sont des <strong>prestataires</strong> engagés pour le devenir de leur cité, 
+<h4 style=\"line-height: 200%; \">Ce sont des <strong>prestataires</strong> engagés pour le devenir de leur cité, 
 qui acceptent les Ropi, et font la démarche de s'approvisioner dans l'économie locale, 
-voire de <strong>trouver de nouvelles fili&egrave;res d'approvisonnement</strong>.</h5>
+voire de <strong>trouver de nouvelles fili&egrave;res d'approvisonnement</strong>.</h4>
 
-<h5 style=\"line-height: 200%; text-align: center; ><strong>Ce sont les consommateurs ET les prestataires, qui ENSEMBLE, 
-avec l'aide du Ropi comme vecteur d'échange, contribuent ainsi à relocaliser et circulariser l'économie.</strong></p>
+<h4 style=\"line-height: 200%; \">Ce sont les consommateurs ET les prestataires, qui <strong>ensemble</strong>, 
+avec l'aide du Ropi comme vecteur d'échange, contribuent ainsi à relocaliser et circulariser l'économie.</p>
 <p><img style=\"display: block; margin-left: auto; margin-right: auto;\" 
 src=\"https://localhost:8000/images/cms/component_circulation.png\" 
 alt=\"\" width=\"400\" /></p>
 
-<p style=\"text-align: center;\">Le fonctionnement du Ropi est détaillé dans le
+<p style=\"line-height: 200%;\">Le fonctionnement du Ropi est détaillé dans le
 <a href=\"https://github.com/RopiMons/RopiDocs/raw/master/ROI/RopiROI.pdf\">R&egrave;glement d'Ordre d'Intérieur</a>.</p>
 
-<p>&nbsp;</h5>";
+<p>&nbsp;</h4>
+
+
+";
--- src/DataFixtures/Paragraphes/depenser.php
+++ src/DataFixtures/Paragraphes/depenser.php
@@ -6,20 +6,20 @@ return  "
 <br>
 <br>
 
-<h4 style=\"line-height: 200%; text-align: center; \">La vocation des Ropi est de circuler, c'est à dire qu'après avoir été injectés dans le circuit par les usagers 
+<h4 style=\"line-height: 200%; \">La vocation des Ropi est de circuler, c'est à dire qu'après avoir été injectés dans le circuit par les usagers 
 (<a href=\"https://www.ropi.be/page/Actions/Commander des Ropi\">acheter des Ropi</a>), 
-les Ropi doivent continuer à circuler de commerçants en producteurs et de producteurs en commerçants.</h4>
+les Ropi doivent continuer à <strong> circuler de commerçants en producteurs</strong> et de producteurs en commerçants.</h4>
 
 <br>
-<h5 style=\"line-height=200%; text-align: center; \">Si un <strong>goulot d'étranglement</strong> se crée, c'est à dire que des Ropi s'accumulent dans le tiroir-caisse 
-d'un commerçant qui n'arrive pas à les depenser, la monnaie ne remplira pas ses objectifs.</h5>
+<h4 style=\"line-height: 200%;  \">Si un <strong>goulot d'étranglement</strong> se crée, c'est à dire que des Ropi s'accumulent dans le tiroir-caisse 
+d'un commerçant qui n'arrive pas à les depenser, la monnaie ne remplira pas ses objectifs.</h4>
 
-<h5 style=\"line-height=200%; text-align: center; \">Mais il existe <strong>pléthore de possibilités pour faire circuler les Ropi.</strong> Nous invitons commerçants 
-et producteurs à les utiliser sans modération! Voyez plutôt :</h5>
+<h4 style=\"line-height: 200%;  \">Mais il existe <strong>pléthore de possibilités pour faire circuler les Ropi.</strong> Nous invitons commerçants 
+et producteurs à les utiliser sans modération! Voyez plutôt :</h4>
 
 <br>
 
-<h5 style=\"line-height: 200%;\">
+<h4 style=\"line-height: 200%;\">
 <ul>
 <li>Trouver des fournisseurs locaux et les payer en Ropi. Utiliser <a href=\"https://www.ropi.be/commerces\">l'outil de recherche</a> 
 des commerçants et producteurs.</li>
@@ -31,26 +31,26 @@ des commerçants et producteurs.</li>
 <li>Rééquilibrer les caisses entre commerçants.</li>
 <li>Payer son repas du midi, une réunion d'affaire en Ropi.</li>
 </ul>
-</h5>
+</h4>
 
 <br>
 
-<h4 style=\"line-height: 200%; text-align: center; \"><strong>Bref, il faut que ça circule !</strong></h4>
+<h3 style=\"line-height: 200%; text-align: center; \"><strong>Bref, il faut que ça circule !</strong></h3>
 
 <br><br>
 
-<h5 style=\"line-height: 200%; text-align: center; \">En enfin, si malgré tout ça il n'est pas possible d'écouler tous ses Ropi, <strong>il reste la possibilité 
+<h4 style=\"line-height: 200%;\">En enfin, si malgré tout ça il n'est pas possible d'écouler tous ses Ropi, <strong>il reste la possibilité 
 de les&nbsp;<a href=\"https://www.ropi.be/page/Actions/Reconvertir%20des%20Ropi\">reconvertir</a> en euros en 
-s'acquitant d'une taxe de <code>5</code> %</strong>.</h5>
+s'acquitant d'une taxe de <code>5</code> %</strong>.</h4>
 
 <br>
 
-<h5 style=\"line-height: 200%; text-align: center \"><strong>En dernier recours</strong>, la<strong> reconversion à 0 % (sans taxe)</strong> est possible 
+<h4 style=\"line-height: 200%; \"><strong>En dernier recours</strong>, la<strong> reconversion à 0 % (sans taxe)</strong> est possible 
 <strong>au dessus d'un certain montant</strong>. En effet, un commerçant qui a vraiment du mal à écouler ses Ropi, 
 peut le signaler à l'asbl qui cherchera alors une solution en collaboration avec le commerçant pour écouler les Ropi. 
 Si aucune solution n'est trouvée endéans les deux semaines, la reconversion à 0% est acceptée pour une durée déterminée,
 <strong>par tranche de <code>100</code> € pour les asbl et <code>200</code> € pour les prestataires du secteur marchand</strong>.
-</h5>
+</h4>
 
 
 ";
